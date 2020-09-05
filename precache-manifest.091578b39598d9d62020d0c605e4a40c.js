6a4174558aec12605b47945b34847de909283dbb
Anteneh Negera <angias.forever@gmail.com>
2020-09-05 14:58:31 -0600

    Updates

---
+++ precache-manifest.091578b39598d9d62020d0c605e4a40c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7394e1a09e0a41ce7ac73ba42c57e8a",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "ee5af798be051ec3a174",
+    "url": "/React-Portfolio/static/js/2.49de80ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.49de80ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a982675aa5c2629e370d",
+    "url": "/React-Portfolio/static/js/main.02448827.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  }
+]);
